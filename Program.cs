ffd9ec8c27c3a93a0a93767cc9da820d8bb2c570
LGRN424 <LGRN424@GMAIL.COM>
2019-08-25 10:52:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J2P_CH3a
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         int secretNum = 95, guessNum;
+
+         Console.WriteLine("Guess a number between 0 and 100: ");
+         guessNum = Int32.Parse(Console.ReadLine());
+
+         if (secretNum == guessNum)
+            Console.WriteLine("Great guess,you win!");
+
+         else if (secretNum > guessNum)
+            Console.WriteLine("Sorry too high!");
+
+         else if (secretNum < guessNum)
+            Console.WriteLine("Sorry too low!");
+
+         else
+            Console.WriteLine("You can play again later.");
+
+
+
+
+         Console.ReadLine();
+        
+      }
+   }
+}
+
